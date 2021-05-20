5cbb08e6cf4ad184f629854317f0913eae1f348c
RAJA <RAJA@Raja.mshome.net>
2021-05-20 19:33:04 +0530

    Embededd DataBase

---
+++ SpringBoot2H2Ex/src/main/java/in/nareshit/raghu/SpringBoot2H2ExApplication.java
@@ -0,0 +1,13 @@
+package in.nareshit.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2H2ExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2H2ExApplication.class, args);
+	}
+
+}
---
+++ SpringBoot2H2Ex/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into product values(10,'book',100);
+insert into product values(11,'pen',200);
+insert into product values(12,'paper',300);
