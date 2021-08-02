8e0cc522bab46322e42ee17a58289cbfa373de85
Raja <durgashankarpatra380@gmail.com>
2021-08-02 07:26:19 +0530

    MySqlCurdEx

---
+++ SpringBoot2WebMvc-MySqlCurdEx/src/main/java/in/nareshit/raghu/SpringBoot2WebMvcMySqlCurdExApplication.java
@@ -0,0 +1,13 @@
+package in.nareshit.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2WebMvcMySqlCurdExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2WebMvcMySqlCurdExApplication.class, args);
+	}
+
+}
